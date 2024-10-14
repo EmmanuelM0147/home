ae7935ef9592f140ff266bc41525147d70e657fe
Emmanuel Okeowo <okeowoemmanuelm@gmail.com>
2024-10-14 17:41:15 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
-  integrations: [tailwind(), compress()],
+  integrations: [
+    sentry({
+      dsn: "https://e0730e67ee927bbbaa3ff84b4e4d2b96@o4508122048757760.ingest.us.sentry.io/4508122058850304",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+    tailwind(),
+    compress(),
+  ],
 });
